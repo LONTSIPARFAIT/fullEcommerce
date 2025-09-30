e66f52dd94d7d3721c67e407b5bc1bdf6384b36f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-30 21:13:47 +0100

    first commit

--- resources/js/components/DataTables/DataTable.tsx
+++ resources/js/components/DataTables/DataTable.tsx
@@ -1,5 +1,5 @@
-import { usePage } from "@inertiajs/react";
-import { useState } from "react";
+import { usePage, router } from "@inertiajs/react";
+import React, { useState } from "react";
 
 interface TableColumn {
     key: string;
@@ -24,12 +24,48 @@ export default function DataTable({
     icon: Icon,
     createRoute = '',
     editRoute = '',
-    ondelete,
+    onDelete,
+}: {
+    data : any;
+    columns: TableColumns[];
+    resourceName: string;
+    singularName: string;
+    routeName: string;
+    filters: any;
+    canViewResources: boolean;
+    canCreateResources: boolean;
+    canEditResources: boolean;
+    canDeleteResources: boolean;
+    icon: React.ElementType;
+    createRoute: string;
+    editRoute: string;
+    onDelete: (id: string) => void;
 }) {
     const { errors } = usePage().props;
 
     const [search, setSearch] = useState(filters?.search || '');
-    const [perPage, SetPerPage] = useState(filters?.perPage || 10);
+    const [perPage, setPerPage] = useState(filters?.perPage || 10);
+    const [sort, setSort] = useState(filters?.sort || 'id');
+    const [direction, setDirection] = useState(filters?.direction || 'desc');
+    
+    const [itemToDelete, setItemToDelete] = useState(null);
+    const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+    const upadateRoute = (newParams = {}) =>{
+        const params = {
+            search,
+            perPage,
+            sort,
+            direction,
+            page: 1,
+            ...newParams
+        };
+        
+        router.get(route(routeName),params, {
+            preserveState: true;
+            preserveScroll: true;
+        });
+    };
 
     return (
         <div>
