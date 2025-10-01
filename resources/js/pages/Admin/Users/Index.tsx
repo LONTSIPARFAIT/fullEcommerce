f8b4ffbe78fc5690abe7d75155e9a0e8df71e192
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-01 10:34:05 +0100

    first commit

--- resources/js/pages/Admin/Users/Index.tsx
+++ resources/js/pages/Admin/Users/Index.tsx
@@ -1,3 +1,10 @@
+import { usePage } from "@inertiajs/react";
+
 export default function UserIndex(){
-    
+    const {users, filters, can } = usePage().props;
+    const columns = [
+        {
+            'key': "index",
+        }
+    ]
 }
