5b6c5c12515f9dc5294eec6c831c62c0f34e21ef
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-01 10:05:29 +0100

    first commit

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class UserController extends Controller
 {
-    //
+    public function index(Request $request): Response{
+        
+        $perPage = $request->input('perPage', 10);
+
+        return Inertia::render('Admin/Users/Index', [
+
+        ]);
+    }
 }
