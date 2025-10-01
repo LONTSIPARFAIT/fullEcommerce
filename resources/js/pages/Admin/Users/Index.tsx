20f6801939cc3bb4b3fd2dceae433719f2d64d03
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-01 10:44:22 +0100

    first commit

--- resources/js/pages/Admin/Users/Index.tsx
+++ resources/js/pages/Admin/Users/Index.tsx
@@ -1,10 +1,23 @@
 import { usePage } from "@inertiajs/react";
+import { Key } from "lucide-react";
 
 export default function UserIndex(){
     const {users, filters, can } = usePage().props;
     const columns = [
         {
-            'key': "index",
-        }
+            key: "index",
+            label: "#",
+            sortable: false,
+            type: 'IndexColumn',
+            width: '80px',
+            render: (item: any, index: number) => {
+                return (filters.page - 1) * filters.page + index + 1 ;
+            }
+        },
+        { Key: 'name', label: 'Name', sortable: true },
+        { Key: 'email', label: 'Email', sortable: true },
+        { Key: 'phone', label: 'Phone', sortable: true },
+        { Key: 'create_at', label: 'Create At', sortable: true },
+        { Key: 'action', label: 'Action', sortable: true },
     ]
 }
