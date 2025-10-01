41f10576ff12478b6ad669ce584a31e942dd85a2
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-01 22:19:26 +0100

    first commit

--- app/Helpers/ImageUploader.php
+++ app/Helpers/ImageUploader.php
@@ -3,16 +3,27 @@
 namespace App\Helpers;
 
 use Illuminate\Http\UploadedFile;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
 
 class ImageUploader
 {
     public function uploadImage(UploadedFile $image, string $folder, int $maxWidth=1200) : string
     {
         try {
+            $filename = uniqid() . '_' . time() .'.webp';
+            $folder=trim($folder, '/');
+            $storagePath="uploads/{$folder}/{$fileName}";
 
+            $manager = new ImageManager(new Driver());
+            $image = $manager->read($image);
+
+            if($image->width() > $maxWidth){
+                $image->scaleDown(width: $maxWith);
+            }
         } catch (\Exception $e) {
             report ($e);
-            return null;
+            return '';
         }
     }
 }
