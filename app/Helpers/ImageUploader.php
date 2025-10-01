d602c7c7bf45b2998dc1249356bf62d88890649c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-01 21:57:32 +0100

    first commit

--- app/Helpers/ImageUploader.php
+++ app/Helpers/ImageUploader.php
@@ -2,10 +2,17 @@
 
 namespace App\Helpers;
 
+use Illuminate\Http\UploadedFile;
+
 class ImageUploader
 {
-    public function uploadImage($image, $folder)
+    public function uploadImage(UploadedFile $image, string $folder, int $maxWidth=1200) : string
     {
-        $
+        try {
+
+        } catch (\Exception $e) {
+            report ($e);
+            return null;
+        }
     }
 }
