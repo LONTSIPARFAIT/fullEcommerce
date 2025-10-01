486433ceee2e92da85c9df66d51ea432f22feb2b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-01 10:27:56 +0100

    first commit

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -10,11 +11,36 @@ use Inertia\Response;
 class UserController extends Controller
 {
     public function index(Request $request): Response{
-        
+
+        $page = $request->input('page', 1);
         $perPage = $request->input('perPage', 10);
+        $search = $request->input('search', '');
+        $sort = $request->input('sort', 'id');
+        $direction = $request->input('direction', 'asc');
+
+        $users = User::select('id', 'name', 'email', 'phone', 'created_at')
+        ->when($search, function ($query, $search) {
+            $query->where('name', 'like', '%'.$search.'%')
+            ->orWhere('email', 'like', '%'.$search.'%')
+            ->orWhere('phone', 'like', '%'.$search.'%');
+        })
+        ->orderBy($sort, $direction)
+        ->paginate($perPage)->withQueryString();
 
         return Inertia::render('Admin/Users/Index', [
-
+            'users' => $users,
+            'filters' => [
+                'search' => $search,
+                'sort' => $sort,
+                'direction' => $direction,
+                'perPage' => $perPage,
+                'page' => $page,
+            ],
+            'can'=>[
+                'create'=>true,
+                'edit'=>true,
+                'delete'=>true,
+            ]
         ]);
     }
 }
