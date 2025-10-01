cc30aba0677226c9d1f089e8b23375583fd1174f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-01 09:58:38 +0100

    first commit

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Middleware\AdminCheckMiddleware;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware(['auth', AdminCheckMiddleware::class])->group(function () {
-    Route::prefix('admin')->name('admin.')->group(function () {});
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::resources([
+            'users' => UserController::class
+        ]);
+    });
 });
