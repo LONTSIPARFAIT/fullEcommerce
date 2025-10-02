151bf274fd4dc85ba2a53717c7d692ad97d49d77
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-02 08:07:10 +0100

    first commit

--- resources/js/pages/Admin/Admins/Create.tsx
+++ resources/js/pages/Admin/Admins/Create.tsx
@@ -1,6 +1,8 @@
+import { Card } from "@/components/ui/card";
+import AppLayout from "@/layouts/app-layout";
 import { BreadcrumbItem } from "@/types";
-import { useForm } from "@inertiajs/react";
-import { useState } from "react";
+import { Head, useForm } from "@inertiajs/react";
+import React, { useRef, useState } from "react";
 
 const breadcrumbs: BreadcrumbItem[] = [
     { title: 'Dashboard', href: 'dashboard' },
@@ -18,43 +20,64 @@ export default function Create(){
         avatar: null as File | null,
     });
     const [imagePreview, setImagePreview] = useState<string | null>(null);
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const [uploadProgress, setUploadProgress] = useState<number>(0);
+    const [isUploading, setIsUploading] = useState<boolean>(false);
 
-    const handleDelete = (id: string) => {
-        router.delete(route('admin.users.destroy', id), {
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setIsUploading(true);
+
+        post(route('admin.admins.store'), {
+            data: {
+                ...data,
+            },
             preserveScroll: true,
-            onSuccess: () => {
-                // toast.success('User delete sucessfuly')
+            onProgress: (progress)=>{
+                if (progress.percentage) {
+                    setUploadProgress(progress.percentage);
+                }
+            },
+            onSuccess: ()=>{
+                setIsUploading(false);
+                setUploadProgress(0)
             },
             onError: ()=>{
-                // toast.success('User deletion failed')                
-            }
+                setIsUploading(false);
+                setUploadProgress(0)
+            },
         })
     }
-    console.log(admins);
+
+    const handleFileChange = (e: React.FormEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0] || null;
+        
+        if (file) {
+            setData('image', file);
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                setImagePreview(e.target?.result as string );
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+
+    const clearImage = () => {
+        setData('image', null);
+        setImagePreview(null);
+        
+        if (fileInputRef.current) {
+            fileInputRef.current.value = '';
+        }
+    }
 
     return (
-        <AppLayout>
-            <Head title="Admins" />
-            <div className="py-6">
-                <div className="mx-auto">
-                    <DataTable
-                      data={admins}
-                      columns={columns}
-                      resourceName="Admins"
-                      singularName="Admin"
-                      routeName="admin.admins.index"
-                      filters={filters}
-                      canViewResource={false}
-                      canCreateResource={true}
-                      canEditResource={true}
-                      canDeleteResource={true}
-                      viewRoute="admin.admins.show"
-                      editRoute="admin.admins.edit"
-                      onDelete={handleDelete}
-                      icon={Users}
-                      createRoute="admin.admins.create"
-                    />                    
-                </div>
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Create Admin" />
+            <div className="from-gray-50 min-h-screen bg-gradient-to-br to-gray-100 p-4 sm:p-6 lg:p-8 dark:from-gray-900 dark:to-gray-800">
+                <Card className="mx-auto">
+                    <CardHearder></CardHearder>
+                </Card>
             </div>
         </AppLayout>
     )
