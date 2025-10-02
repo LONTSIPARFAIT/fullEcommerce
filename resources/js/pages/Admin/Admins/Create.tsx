75da61a00bbd01c06457b800a8e75ada6c234722
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-02 19:52:58 +0100

    first commit

--- resources/js/pages/Admin/Admins/Create.tsx
+++ resources/js/pages/Admin/Admins/Create.tsx
@@ -179,12 +179,12 @@ export default function Create() {
                                     <Upload size={24} className='mb-2 text-gray-500 dark:text-gray-400' />
                                     <p className="text-sm font-medium text-gray-700 dark:text-gray-300">Click to upload</p>
                                     <p className="mt-1 text-xs text-gray-500 dark:text-gray-400">
-                                        {t('PNG, JPG, GIF up to SMB')}
+                                        PNG, JPG, GIF up to SMB'
                                     </p>
                                 </div>
                             ) : (
                                 <div className="relative h-40 w-full overflow-hidden rounded-lg border border-gray-200 bg-white/80 transition-all dark:border-gray-600 dark:bg-gray-800/80">
-                                    
+                                    <img src="" alt="" />
                                 </div>
                             )}
                         </div>
