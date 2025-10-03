306125c737e75f9dab20a50bbfeba296517d4732
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-03 07:50:35 +0100

    erreur vite

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'image',
+        'description',
+        'parent_id',
+    ];
+
+    public function parent(){
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }
