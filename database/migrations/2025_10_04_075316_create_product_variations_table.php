7725d718a57bcf41eeb45c9c1943e661fac999b9
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-04 09:15:52 +0100

    first commit

--- database/migrations/2025_10_04_075316_create_product_variations_table.php
+++ database/migrations/2025_10_04_075316_create_product_variations_table.php
@@ -21,14 +21,18 @@ return new class extends Migration
 
         Schema::create('variation_type_options', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('variation_type_id')->index()->constrained('products')->cascadeOnDelete();
+            $table->foreignId('variation_type_id')->index()->constrained('variation_types')->cascadeOnDelete();
             $table->string('name', 2000);
-            $table->string('type', 2000);
             $table->timestamps();
         });
 
         Schema::create('product_variations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->index()->constrained('products')->cascadeOnDelete();
+            $table->json('variation_type_option_ids');
+            $table->integer('quantity')->default(0);
+            $table->decimal('price', 20, 2)->default(0);
+            $table->string('sku', 2000);
             $table->timestamps();
         });
     }
