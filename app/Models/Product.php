0d6c4545a2d4d8f928aa901bd5d853f9dc8da0c2
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-04 16:53:02 +0100

    first commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -49,6 +49,10 @@ class Product extends Model implements HasMedia
 
     public function options()
     {
-        return $this->hasManyThrough(VariationTypeOption::class);
+        return $this->hasManyThrough(VariationTypeOption::class, 'product_id',  'variation_type_id', 'id', 'id');
+    }
+
+    public function variations(){
+        return $this->hasMany(ProductVariation::class);
     }
 }
