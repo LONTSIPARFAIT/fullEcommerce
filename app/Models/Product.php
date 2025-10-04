729b309eef998a6e484112aa0c92d4c701493860
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-04 10:37:53 +0100

    first commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
-    use HasSlug;
+    use HasSlug, InteractsWithMedia;
     protected $guarded = [];
         public function getSlugOptions(): SlugOptions{
         return SlugOptions::create()
