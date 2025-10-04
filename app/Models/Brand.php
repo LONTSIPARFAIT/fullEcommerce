7bc4df4a25916fe5ab250d0d6658d8bd04fe5f56
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-04 07:16:41 +0100

    first commit

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,13 +2,21 @@
 
 namespace App\Models;
 
+use Spatie\Sluggable\HasSlug;
 use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
+    use HasSlug;
        protected $fillable = [
         'name',
         'slug',
         'image',
+        'status',
     ];
+    protected $casts = [
+        'status' => 'boolean',
+    ];
+
+    public function getUrlImage
 }
