438c0a04c422066ce8078a857839ea535ba05dd6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-04 10:33:04 +0100

    first commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
-    //
+    use HasSlug;
+    protected $guarded = [];
+        public function getSlugOptions(): SlugOptions{
+        return SlugOptions::create()
+        ->generateSlugsFrom('name')
+        ->saveSlugsTo('slug');
+    }
 }
