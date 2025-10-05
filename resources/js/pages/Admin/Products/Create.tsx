b9800e6134f7d447bac09600ad27b65f1f5df1b3
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-05 14:23:07 +0100

    first commit

--- resources/js/pages/Admin/Products/Create.tsx
+++ resources/js/pages/Admin/Products/Create.tsx
@@ -34,6 +34,11 @@ interface Props {
     brands: Brand[];
 }
 
+const statusOptions = [
+    { label: 'Draft', value: 'draft' },
+    { label: 'Published', value: 'published' },
+]
+
 export default function Create({categories, brands}: Props) {
     // console.log(categories);
     // console.log(brands);
@@ -222,7 +227,7 @@ export default function Create({categories, brands}: Props) {
                                 )}
                             </div>
                         </div>
-                        {/* Pricing */}
+                        {/* Pricing & quantity & status */}
                         <div className="grid gap-6 md:grid-cols-3">
                             {/* price */}
                             <div className="space-y-2">
@@ -270,12 +275,11 @@ export default function Create({categories, brands}: Props) {
                                 <div className="group relative">
                                     <Input 
                                     id='quantity'
-                                    type='number'
                                     value={data.quantity }
                                     onChange={(e)=>setData('quantity', e.target.value)}
                                     //   step='0.01'
                                     className='focus:border-primary focus:ring-primary/20 dark:focus:ring-primary-foreground/20 h-12 w-full rounded-lg border border-gray-200 bg-white/80 pl-10 text-base text-gray-900 shadow-sm backdrop-blur-sm transition-all group-hover:border-gray-300 focus:ring-2 dark:border-gray-600 dark:bg-gray-800/80 dark:text-gray-100 dark:group-hover:border-gray-500 dark:focus:border-primary-foreground'
-                                    placeholder='0'
+                                    placeholder='Available quantity'
                                     />
                                     <File
                                     size={18}
@@ -290,7 +294,7 @@ export default function Create({categories, brands}: Props) {
                                     </div>
                                 )}
                             </div>
-                            {/*  */}
+                            {/* status */}
                             <div className="space-y-2">
                                 <Label htmlFor='status' className='flex items-center gap-2 text-sm font-medium text-gray-700 dark:text-gray-200'>
                                     <List size={14} className='text-primary dark:text-primary-foreground'/>
@@ -303,8 +307,8 @@ export default function Create({categories, brands}: Props) {
                                     </SelectTrigger>
                                     <SelectContent>
                                         {statusOptions.map((option)=>(
-                                            <SelectItem key={option.id} value={option.id.toString()}>
-                                                {option.path}
+                                            <SelectItem key={option.value} value={option.value}>
+                                                {option.label}
                                             </SelectItem>
                                         ))}
                                     </SelectContent>
@@ -314,6 +318,71 @@ export default function Create({categories, brands}: Props) {
                                     <div className="mt-2 flex items-center gap-6 rounded-md bg-red-50 p-2 text-sm text-red-500 dark:text-red-400">
                                         <AlertCircle size={14} />
                                         <span>{errors.status}</span>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                        {/* SKU and barcode */}
+                        <div className="grid gap-6 md:grid-cols-2">
+                            {/* SKU */}
+                            <div className="space-y-2">
+                                <Label
+                                  htmlFor="sku" 
+                                  className="flex items-center gap-2 text-sm font-medium text-gray-700 dark:text-gray-200"
+                                >
+                                    <TagIcon size={14} className="text-primary dark:text-primary-foreground" />
+                                    SKU
+                                </Label>
+
+                                <div className="group relative">
+                                    <Input 
+                                    id='sku'
+                                    value={data.sku}
+                                    onChange={(e)=>setData('sku', e.target.value)}
+                                    className='focus:border-primary focus:ring-primary/20 dark:focus:ring-primary-foreground/20 h-12 w-full rounded-lg border border-gray-200 bg-white/80 pl-10 text-base text-gray-900 shadow-sm backdrop-blur-sm transition-all group-hover:border-gray-300 focus:ring-2 dark:border-gray-600 dark:bg-gray-800/80 dark:text-gray-100 dark:group-hover:border-gray-500 dark:focus:border-primary-foreground'
+                                    placeholder='Enter SKU'
+                                    />
+                                    <File
+                                    size={18}
+                                    className="group-hover:text-primary absolute top-1/2 left-3 -translate-y-1/2 text-gray-400 transition-colors dark:text-gray-500 dark:group-hover:text-primary-foreground"
+                                    />
+                                </div>
+
+                                {errors.sku && (
+                                    <div className="mt-2 flex items-center gap-6 rounded-md bg-red-50 p-2 text-sm text-red-500 dark:text-red-400">
+                                        <AlertCircle size={14} />
+                                        <span>{errors.sku}</span>
+                                    </div>
+                                )}
+                            </div>
+                            {/* barcode */}
+                            <div className="space-y-2">
+                                <Label
+                                  htmlFor="barcode" 
+                                  className="flex items-center gap-2 text-sm font-medium text-gray-700 dark:text-gray-200"
+                                >
+                                    <TagIcon size={14} className="text-primary dark:text-primary-foreground" />
+                                    Barcode
+                                </Label>
+
+                                <div className="group relative">
+                                    <Input 
+                                    id='barcode'
+                                    value={data.barcode }
+                                    onChange={(e)=>setData('barcode', e.target.value)}
+                                    className='focus:border-primary focus:ring-primary/20 dark:focus:ring-primary-foreground/20 h-12 w-full rounded-lg border border-gray-200 bg-white/80 pl-10 text-base text-gray-900 shadow-sm backdrop-blur-sm transition-all group-hover:border-gray-300 focus:ring-2 dark:border-gray-600 dark:bg-gray-800/80 dark:text-gray-100 dark:group-hover:border-gray-500 dark:focus:border-primary-foreground'
+                                    placeholder='Enter barcode'
+                                    />
+                                    <File
+                                    size={18}
+                                    className="group-hover:text-primary absolute top-1/2 left-3 -translate-y-1/2 text-gray-400 transition-colors dark:text-gray-500 dark:group-hover:text-primary-foreground"
+                                    />
+                                </div>
+
+                                {errors.barcode && (
+                                    <div className="mt-2 flex items-center gap-6 rounded-md bg-red-50 p-2 text-sm text-red-500 dark:text-red-400">
+                                        <AlertCircle size={14} />
+                                        <span>{errors.barcode}</span>
                                     </div>
                                 )}
                             </div>
