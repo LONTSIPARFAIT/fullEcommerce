0118355ac81f8402a506843c122ba5292a2f87fd
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-07 22:28:31 +0100

    first commit

--- app/Http/Controllers/Admin/ProductImageController.php
+++ app/Http/Controllers/Admin/ProductImageController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ProductImageController extends Controller
 {
-    public function index(){
+    public function index(Request $request, $id){
+        $product= Product::findOrFail($id); 
         $images = [];
+        // dd($product); 
 
-        return Inertia::render('Admin/Products/Images/index',[
+        return Inertia::render('Admin/Products/Images/Index',[
             'images' => $images,
+            'product' => $product,
         ]);
     }
 
