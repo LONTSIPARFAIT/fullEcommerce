6e55a56f640956da4c172540fc56f59ad772dcc4
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-07 22:36:37 +0100

    first commit

--- app/Http/Controllers/Admin/ProductVariationController.php
+++ app/Http/Controllers/Admin/ProductVariationController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProductVariationController extends Controller
 {
-    //
+    public function index(Request $request, $id){
+        $product= Product::findOrFail($id); 
+        $images = [];
+        // dd($product); 
+
+        return Inertia::render('Admin/Products/Images/Index',[
+            'images' => $images,
+            'product' => $product,
+        ]);
+    }
+
+    public function store(){
+        //
+    }
+
+    public function update(Request $request){
+        //
+    }
+
+    public function destroy(){
+        //
+    }
 }
