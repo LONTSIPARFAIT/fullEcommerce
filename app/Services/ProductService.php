7c9db7218c9c9478c2cb36ade2a5b399b1651b1e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-10 15:10:47 +0100

    first commit

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -3,5 +3,28 @@ namespace App\Services;
 
 class ProductService
 {
- public function mergeCartesianWithExisting(){}
+ public function mergeCartesianWithExisting($variationTypes, $existingData, $product){
+    $defaultQuantity = $product->quantity;
+    $defaultPrice = $product->price;
+    $cartesianProduct = $this->cartesianProduct($variationTypes, $defaultQuantity, $defaultPrice);
+    $mergedResult = [];
+
+    foreach ($cartesianProduct as $product) {
+        $optionIds = collect($product)->filter(fn($value, $key) => str_starts_with($key, 'variation_type_'))->map(fn($option)=>$option['id'])->values()->toArray();
+
+        $match = array_filter($existingData, function ($existingOption) use ($optionIds) {
+            return $existingOption['variation_type_option_ids'] === $optionIds;
+        });
+
+        if (!empty($match)) {
+            $existingEntry = reset($match);
+            $product['id'] = $existingEntry['id'];
+            $product['quantity'] = $existingEntry['quantity'];
+            $product['price'] = $existingEntry['price'];
+        }else {
+            $product['quantity'] = $defaultQuantity;
+            $product['price'] = $defaultPrice;
+        }
+    }
+ }
 }
