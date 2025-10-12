946de2b1c420d6edafdf391c0fc4468a5e059c0c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-12 12:02:15 +0100

    first commit

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,8 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word(),
+            'slug' => $this->faker->slug(),
         ];
     }
 }
