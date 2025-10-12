ec522c7eded959a8c3217cb05d1d6f5a3e84abb3
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-12 15:45:49 +0100

    first commit

--- resources/js/components/ecommerces/Header.tsx
+++ resources/js/components/ecommerces/Header.tsx
@@ -14,15 +14,9 @@ export default function Head() {
       <div className="container mx-auto px-4 flex justify-between items-center">
         <div className="flex space-x-4">
           <Link prefetch href="#" className="hover:text-gray-300"><i className="fab fa-facebook-f"></i></Link>
-          <Link prefetch href="#" className="hover:text-gray-300">
-            <i className="fab fa-twitter"></i>
-          </Link>
-          <Link prefetch href="#" className="hover:text-gray-300">
-            <i className="fab fa-intagram"></i>
-          </Link>
-          <Link prefetch href="#" className="hover:text-gray-300">
-            <i className="fab fa-pinterest"></i>
-          </Link>
+          <Link prefetch href="#" className="hover:text-gray-300"><i className="fab fa-twitter"></i></Link>
+          <Link prefetch href="#" className="hover:text-gray-300"><i className="fab fa-intagram"></i></Link>
+          <Link prefetch href="#" className="hover:text-gray-300"><i className="fab fa-pinterest"></i></Link>
         </div>
         <div className="flex items-center space-x-6">
           <div className="relative" x-data="{ open: false }">
