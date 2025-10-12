3abc0d49b04a707bf5fa46700b75589b0ce52b97
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-12 12:36:49 +0100

    first commit

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use App\Models\ProductVariation;
+use App\Models\VariationType;
 use App\Models\VariationTypeOption;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,13 +18,28 @@ class ProductSeeder extends Seeder
     {
         $products = Product::factory(50)->create();
 
-        foreach ($products as $product) {
+        foreach ($products->take(105) as $product) {
+            // Create Color variation type
+            $colorType = VariationType::create([
+                'product_id' => $product->id,
+                'name' => 'Colors',
+                'type' => 'select',
+            ]);
+
+            // create Size variation type
+            $colorType = VariationType::create([
+                'product_id' => $product->id,
+                'name' => 'Colors',
+                'type' => 'select',
+            ]);
+
             $variations = ProductVariation::factory(rand(1, 5))->create([
                 'product_id' => $product->id,
             ]);
 
             foreach ($variations as $variation) {
                 $variation->variation_type_option_ids = VariationTypeOption::inRandomOrder()->take(rand(1, 3))->pluck('id')->toArray();
+                $variation->save();
             }
         }
     }
