8aee2edec9b142cbed078e14810fda071a7d0fd3
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-12 12:00:07 +0100

    first commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\ProductStatusEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -12,7 +13,7 @@ use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model implements HasMedia
 {
-    use HasSlug, InteractsWithMedia;
+    use HasSlug, InteractsWithMedia, HasFactory;
     protected $guarded = [];
         public function getSlugOptions(): SlugOptions{
         return SlugOptions::create()
