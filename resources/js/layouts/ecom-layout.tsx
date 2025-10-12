f6ec8c848a9a0e86f68cd6cb0efdd6263a8903ba
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-12 14:50:03 +0100

    first commit

--- resources/js/layouts/ecom-layout.tsx
+++ resources/js/layouts/ecom-layout.tsx
@@ -1,11 +1,25 @@
+import { Head } from "@inertiajs/react";
 
-interface EcommerceLayoutProps{
-    children: React.ReactNode;
-    title?: string;
+interface EcommerceLayoutProps {
+  children: React.ReactNode;
+  title?: string;
 }
 
-export default function ({ children, title="R-Mart" }:EcommerceLayoutProps){
-    return (
-        <div className=""></div>
-    );
+export default function ({ children, title = "R-Mart" }: EcommerceLayoutProps) {
+  return (
+    <>
+      <Head >
+        <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="R-Mark - Your one-stop online shopping destination" />
+      </Head>
+      <main className="ecom-content">
+        {children}
+      </main>
+      <footer className="ecom-footer">
+        <p>&copy; 2025 </p>
+      </footer>
+
+    </>
+  );
 }
