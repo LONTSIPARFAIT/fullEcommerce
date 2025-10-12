8104977672dffa6583f9c9f342a75a66efe9a534
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-12 15:29:23 +0100

    first commit

--- resources/js/components/ecommerces/Header.tsx
+++ resources/js/components/ecommerces/Header.tsx
@@ -1,8 +1,13 @@
 import { Link } from "@inertiajs/react";
+import { ChevronDown } from "lucide-react";
+import { useState } from "react";
 
 
 export default function Head() {
-  const {isCardOpen, setIsCardOpen };
+  const [ isCardOpen, setIsCardOpen ] = useState(false);
+  const [ isCurrencyOpen, setIsCurrencyOpen ] = useState(false);
+  const [ isLanguageOpen, setIsLanguageOpen]  = useState(false);
+
   return (    
     // Top Header with social links, currently, language
     <div className="bg-gray-800 text-white py-2">
@@ -22,7 +27,26 @@ export default function Head() {
           </Link>
         </div>
         <div className="flex items-center space-x-6">
-          <div className="relative" x-data="{ open: false }"></div>
+          <div className="relative" x-data="{ open: false }">
+            <button
+              onClick={()=>setIsCurrencyOpen(!isCurrencyOpen)}
+              className="flex items-center space-x-1 hover:text-gray-300"
+            >
+              <span>FCFA</span>
+              <i className="fas fa-chevron-down text-xs"></i>
+              {/* <ChevronDown size={16} /> */}
+            </button>
+            {isCurrencyOpen && ( 
+              <div 
+                x-show="open"
+                className="absolute right-0 mt-2 w-40 bg-white text-gray-800 shadow-lg rounded-md overflow-hidden z-50"
+              >
+                <Link href="#" className="block px-4 py-2 hover:bg-gray-100">FCFA - FR Dollar</Link>
+                <Link href="#" className="block px-4 py-2 hover:bg-gray-100">USD -US Dollar</Link>
+                <Link href="#" className="block px-4 py-2 hover:bg-gray-100">EUR - Euro</Link>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
