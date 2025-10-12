b6ad5a9f1bd8870cf4c8f96e39bd03511a3ace64
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-10-12 22:14:18 +0100

    first commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function index(){
-        return 
+        return Inertia::render('Ecommerce/Home', [
+            'title' => 'Welcome to our store',
+            'description' => 'Explore our wide range of products and enjoy exclusive offers'
+        ]);
     }
 }
---
+++ resources/js/pages/Ecommerce/Home.tsx
@@ -0,0 +1,12 @@
+import EcomLayout from '@/layouts/ecom-layout'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <EcomLayout>
+        <h1>Je suis la page d'acceuil</h1>
+    </EcomLayout>
+  )
+}
+
+export default Home
