1f43cc9c2b017b551dcf0d2366186cae3b5276da
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-11-25 19:00:52 +0100

    first commit

--- resources/js/components/ecommerces/HomePage/BannerAndSlider.tsx
+++ resources/js/components/ecommerces/HomePage/BannerAndSlider.tsx
@@ -1,5 +1,13 @@
+
 export default function BannerAndSlider() {
-    <div className="relative bg-ggray-100">
-        .container
+   return (
+    <div className="container mx-auto px-4 py-8">
+        <div className="flex flex-wrap -mx-4">
+            {/* Left Slide Swipper slider */}
+            <div className="w-full lg:w-3/4 px-4 mb-8 lg:mb-0">
+                <div className="swiper main-slider rounded-lg"></div>
+            </div>
+        </div>
     </div>
+    )
 }
--- resources/js/pages/Ecommerce/Home.tsx
+++ resources/js/pages/Ecommerce/Home.tsx
@@ -1,10 +1,10 @@
-import EcomLayout from '@/layouts/ecom-layout'
-import BannerAndSlider from '@/components/ecommerces/Homepage/BannerAndSlider'
+import BannerAndSlider from '@/components/ecommerces/HomePage/BannerAndSlider'
+import EcomLayout from '@/layouts/ecom-layout'     
 import React from 'react'
 
 export default function Home() {
   return (
-    <EcomLayout>
+    <EcomLayout>    
         <BannerAndSlider />
     </EcomLayout>
   )
