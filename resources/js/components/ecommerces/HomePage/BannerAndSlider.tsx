2f0e09f4684e9daab77d3e08a03cc80f4b37026e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-11-25 21:06:46 +0100

    first commit

--- resources/js/components/ecommerces/HomePage/BannerAndSlider.tsx
+++ resources/js/components/ecommerces/HomePage/BannerAndSlider.tsx
@@ -1,11 +1,19 @@
 
+// import swiper styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/scrollbar';
+
 export default function BannerAndSlider() {
    return (
     <div className="container mx-auto px-4 py-8">
         <div className="flex flex-wrap -mx-4">
             {/* Left Slide Swipper slider */}
             <div className="w-full lg:w-3/4 px-4 mb-8 lg:mb-0">
-                <div className="swiper main-slider rounded-lg"></div>
+                <Swiper
+                  module={Navigation, Pagination, Scrollbar, Autoplay}
+                />
             </div>
         </div>
     </div>
