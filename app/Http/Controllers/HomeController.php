255f34771ffea7f79fb900ae391019c4d9356a4e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-12-02 21:08:39 +0100

    first commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductListResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -9,11 +10,13 @@ use Inertia\Inertia;
 class HomeController extends Controller
 {
     public function index(){
-        $bestSellingProducts = Product::query()->limit(4)->orderBy('sales', 'desc')->get();
-        $specialOffers = Product::query()->where('is_special_offer', true)->limit(4)->get();
+        $bestSellingProducts = ProductListResource::collection(Product::query()->limit(4)->orderBy('sales', 'desc')->get());
+        $specialOffers = ProductListResource::collection(Product::query()->where('is_special_offer', true)->limit(4)->get());
         return Inertia::render('Ecommerce/Home', [
             'title' => 'Welcome to our store',
-            'description' => 'Explore our wide range of products and enjoy exclusive offers'
+            'description' => 'Explore our wide range of products and enjoy exclusive offers',
+            'bestSellingProducts' => $bestSellingProducts,
+            'specialOffers' => $specialOffers,
         ]);
     }
 }
