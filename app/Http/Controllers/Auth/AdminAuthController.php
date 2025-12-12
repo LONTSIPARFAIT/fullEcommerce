bbe64136cba51c69d5d43b766aa7585114d64053
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-12-12 14:03:13 +0100

    first commit

--- app/Http/Controllers/Auth/AdminAuthController.php
+++ app/Http/Controllers/Auth/AdminAuthController.php
@@ -4,8 +4,47 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class AdminAuthController extends Controller
 {
-    //
+    /**
+     * Show the admin page
+     */
+    public function  showLogin() {
+        return Inertia::render('auth/login', [
+            'isAdmin' => true,
+        ]);
+    }
+
+    /**
+     * Handle admin login
+     */
+    public function  login(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            // check if user is admin
+            if($user->role !== 'admin') {
+                Auth::logout();
+                return back()->withErrors([
+                    'email' => 'You are not autorize as admin.',
+                ]);
+            }
+
+            $request->session()->regenerate();
+            return redirect;
+        }
+        return Inertia::render('auth/login', [
+            'isAdmin' => true,
+        ]);
+    }
 }
