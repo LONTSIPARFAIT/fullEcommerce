3321ebb75fe3e700fa7e8cf27e94b3e0bce3d045
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-12-12 12:42:42 +0100

    first commit

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -1,46 +1,66 @@
 import EcomLayout from "@/layouts/ecom-layout";
-import { login, register } from "@/routes";
+import { login, } from "@/routes";
 import { Link, useForm } from "@inertiajs/react";
 import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 
 export default function CustomerLogin() {
     const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     const { data, setData, post, processing, errors } = useForm({
+        name: '',
         email: '',
+        phone: '',
         password: '',
+        password_confirmation: '',
     }); 
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        post('/login');
+        post('/register');
     }
 
     return (
-        <EcomLayout title="Custumer Login - ShopMart">
+        <EcomLayout title="Custumer Register - ShopMart">
             <div className="container mx-auto px-4 py-8">
                 <div className="flex flex-col items-center justify-center gap-8 md:flex-row">
-                    {/* Login Form */}
+                    {/* Register Form */}
                     <div className="md:w-1/2">
                         <div className="overflow-hidden rounded-lg bg-white shadow-sm">
                             <div className="border-b p-6">
-                                <h2 className="text-lg font-semibold">Login</h2>
-                                <p className="mt-1 text-gray-600">Welcome back! Sign in to your account.</p>
+                                <h2 className="text-lg font-semibold">Register</h2>
+                                <p className="mt-1 text-gray-600">Create your account to start shopping!</p>
                             </div>
                             <div className="p-6">
                                 <form onSubmit={handleSubmit} className="">
-                                    <div className="mb-4">
-                                        <label htmlFor="login-email" className="mb-2 block text-sm font-medium text-gray-700">Email Address</label>
-                                        <input 
-                                          type="email"
-                                          id="login-email"
-                                          value={data.email}
-                                          onChange={(e) => setData('email', e.target.value)}
-                                          className="w-full rounded-md border border-gray-300 px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-                                          placeholder="your@gmail.com"
-                                          required />
-                                          {errors.email && <p className="mt-1 text-sm text-red-600">{errors.email}</p>}
+                                    <div className="mb-4 flex gap-2">
+                                        <div className="w-full">
+                                            <label htmlFor="name" className="mb-2 block text-sm font-medium text-gray-700">Full Name</label>
+                                            <input 
+                                            type="text"
+                                            id="name"
+                                            value={data.name}
+                                            onChange={(e) => setData('name', e.target.value)}
+                                            className="w-full rounded-md border border-gray-300 px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+                                            placeholder="Enter Your Name"
+                                            required />
+                                            {errors.name && <p className="mt-1 text-sm text-red-600">{errors.name}</p>}
+                                        </div>
+                                        
+                                        <div className="mb-4">
+                                            <label htmlFor="login-email" className="mb-2 block text-sm font-medium text-gray-700">Email Address</label>
+                                            <input 
+                                            type="email"
+                                            id="login-email"
+                                            value={data.email}
+                                            onChange={(e) => setData('email', e.target.value)}
+                                            className="w-full rounded-md border border-gray-300 px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+                                            placeholder="your@gmail.com"
+                                            required />
+                                            {errors.email && <p className="mt-1 text-sm text-red-600">{errors.email}</p>}
+                                        </div>
+                                    
                                     </div>
                                     <div className="mb-4">
                                         <div className="flex items-center justify-between">
