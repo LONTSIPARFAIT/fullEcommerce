0fc8d3f09afe87b430e17802765b4c2fec67e395
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-12-12 11:49:15 +0100

    first commit

--- resources/js/pages/auth/userlogin.tsx
+++ resources/js/pages/auth/userlogin.tsx
@@ -1,5 +1,6 @@
 import EcomLayout from "@/layouts/ecom-layout";
-import { useForm } from "@inertiajs/react";
+import { Link, useForm } from "@inertiajs/react";
+import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 
 export default function CustomerLogin() {
@@ -41,17 +42,44 @@ export default function CustomerLogin() {
                                           {errors.email && <p className="mt-1 text-sm text-red-600">{errors.email}</p>}
                                     </div>
                                     <div className="mb-4">
-                                        <label htmlFor="login-email" className="mb-2 block text-sm font-medium text-gray-700">Email Address</label>
-                                        <input 
-                                          type="email"
-                                          id="login-email"
-                                          value={data.email}
-                                          onChange={(e) => setData('email', e.target.value)}
-                                          className="w-full rounded-md border border-gray-300 px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-                                          placeholder="your@gmail.com"
-                                          required />
-                                          {errors.email && <p className="mt-1 text-sm text-red-600">{errors.email}</p>}
+                                        <div className="flex items-center justify-between">
+                                            <label htmlFor="login-password" className="mb-2 block text-sm font-medium text-gray-700">Password</label>
+                                            <Link href='/forgot-password' className="text-sm text-indigo-600 hover:text-indigo-800">Forgot Password ?</Link>
+
+                                        </div>
+                                        <div className="relative">
+                                            <input 
+                                            type={showPassword ? 'text' : 'password'}
+                                            id="login-password"
+                                            value={data.password}
+                                            onChange={(e) => setData('password', e.target.value)}
+                                            className="w-full rounded-md border border-gray-300 px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+                                            placeholder="*********"
+                                            required />
+                                            <button
+                                              type="button"
+                                              onChange={(e) => setShowPassword(!showPassword)}
+                                              className="absolute top-1/2 right-3 -translate-y-1/2 transform text-gray-500 hover:text-gray-700" >
+                                                { showPassword ? <EyeOff size={16} /> : <Eye size={16} />}
+                                              </button>
+                                        </div>
+                                        {errors.password && <p className="mt-1 text-sm text-red-600">{errors.password}</p>}
                                     </div>
+                                    <div className="mb-6 flex items-center">
+                                        <input 
+                                          type="checkbox" 
+                                          id="remember-me"
+                                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+                                        <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-700">
+                                            Remember me
+                                        </label>
+                                    </div>
+                                    <button 
+                                      type="submit"
+                                      disabled={processing}
+                                      className="w-full rounded-md bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50">
+                                        {processing ? 'Signing In...' : 'Sign In'}
+                                      </button>
                                 </form>
                             </div>
                         </div>
