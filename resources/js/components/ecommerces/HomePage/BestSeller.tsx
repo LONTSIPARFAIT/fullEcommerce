8caaf5a190b60fe2d2209a1758ce16d03ebf9172
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-12-15 16:20:34 +0100

    first commit

--- resources/js/components/ecommerces/HomePage/BestSeller.tsx
+++ resources/js/components/ecommerces/HomePage/BestSeller.tsx
@@ -5,8 +5,8 @@ import { ProductCard } from "../ProductCard";
 export default function BestSeller() {
     const { bestSellingProducts } = usePage().props as any;
     // console.log('bestSellingProducts',bestSellingProducts);
-    
-  return (
+
+  retur (
     <div className="bg-white py-12">
       <div className="container mx-auto px-4">
         <div className="container flex justify-between items-center mx-auto px-4">
@@ -14,7 +14,7 @@ export default function BestSeller() {
           <Link prefetch href="#" className="text-indigo-600 hover:text-indigo-800">View All</Link>
         </div>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {bestSellingProducts.length > 0 ? 
+            {bestSellingProducts.length > 0 ?
                 (bestSellingProducts.map((product:any)=> <ProductCard key={product.id} {...product} />)
             ) : (
                 <div className="col-span-4 text-center text-gray-500">No best Seller available at the moment.</div>
