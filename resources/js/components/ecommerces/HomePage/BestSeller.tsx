c47e58a0de2578d0db85f8737cec7259ee556a59
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-01-01 21:41:58 +0100

    first commit

--- resources/js/components/ecommerces/HomePage/BestSeller.tsx
+++ resources/js/components/ecommerces/HomePage/BestSeller.tsx
@@ -17,7 +17,7 @@ export default function BestSeller() {
             {bestSellingProducts.length > 0 ?
                 (bestSellingProducts.map((product:any)=> <ProductCard key={product.id} {...product} />)
             ) : (
-                <div className="col-span-4 text-center text-gray-500">No be Seller available at the moment.</div>
+                <div className="col-span-4 text-center text-gray-500">No bes Seller available at the moment.</div>
             )}
         </div>
       </div>
